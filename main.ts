47ddc4aeb918e263e6a9bff224ea15ecd7affba4
uryel704 <135332858+uryel704@users.noreply.github.com>
2023-06-02 13:54:45 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    jugador_arriba.change(LedSpriteProperty.Y, -1)
+    jugador_abajo.change(LedSpriteProperty.Y, -1)
+    basic.pause(500)
+    jugador_arriba.change(LedSpriteProperty.Y, 1)
+    jugador_abajo.change(LedSpriteProperty.Y, 1)
+})
+let jugador_abajo: game.LedSprite = null
+let jugador_arriba: game.LedSprite = null
+jugador_arriba = game.createSprite(1, 3)
+jugador_abajo = game.createSprite(1, 4)
+let obstaculo = game.createSprite(4, 4)
 basic.forever(function () {
-	
+    obstaculo.change(LedSpriteProperty.X, -1)
+    basic.pause(500)
+    if (true) {
+    	
+    }
 })
